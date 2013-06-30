4bb8af2609a0af43e77005022becad5b02c43372
Médéric Andrieux <mederic@mint-mederic.(none)>
2013-06-30 14:03:28 +0200

    admin pages refont

--- app/models/User.java
+++ app/models/User.java
@@ -15,44 +15,54 @@ import play.db.jpa.Model;
 
 @Entity
 public class User extends Model {
-	
+
     @Required
-    @Column(name="name", unique=true)
+    @CheckWith(UniqueUsernameCheck.class)
+    @Column(name = "name", unique = true)
     public String username;
     
     @Required
     @Email
     public String email;
-    
     @Required
     public String password;
-    
     @Required
     public boolean isAdmin;
-    
-    @OneToMany(mappedBy="user", cascade=CascadeType.ALL) 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     public Set<Role> attachedRoles;
-    
+
     @Override
     public String toString() {
-    	return "[" + this.getId() + "] " + username;
+        return "[" + this.getId() + "] " + username;
     }
 
-	public List<Project> getProjects() {
-		List<Role> roles = Role.find("byUser", this).fetch();
-		List<Project> result = new ArrayList<Project>();
-		
-		for (Role role : roles)
-			result.add(role.project);
-		
-		return result;
-	}
+    public List<Project> getProjects() {
+        List<Role> roles = Role.find("byUser", this).fetch();
+        List<Project> result = new ArrayList<Project>();
 
-	public RoleType getRoleTypeFor(models.Project project) {
-		Role currentRole = Role.find("byUserAndProject", this, project).first();
-		if (currentRole != null)
-			return currentRole.role;
-		else
-			return null;
-	}
+        for (Role role : roles) {
+            result.add(role.project);
+        }
+
+        return result;
+    }
+
+    public RoleType getRoleTypeFor(models.Project project) {
+        Role currentRole = Role.find("byUserAndProject", this, project).first();
+        if (currentRole != null) {
+            return currentRole.role;
+        } else {
+            return null;
+        }
+    }
+
+    static class UniqueUsernameCheck extends Check {
+
+        public boolean isSatisfied(Object userObject, Object username) {
+            setMessage("Username already exists...");
+            User user = (User) userObject;
+            User existingUser = User.find("byUsername", username).first();
+            return ((existingUser == null) || (existingUser.id == user.id));
+        }
+    }
 }
